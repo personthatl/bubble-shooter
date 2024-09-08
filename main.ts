56ebd33f5fde5a273b4d7eb619004e60228abf20
personthatl <142671923+personthatl@users.noreply.github.com>
2024-09-08 16:51:07 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    bubble.tossBubble()
+    bubble.load_bubble()
+})
+controller.right.onEvent(ControllerButtonEvent.Repeated, function () {
+    bubble.tilt_angle(bubble.Choice.Right)
+})
+scene.onHitWall(SpriteKind.Bubble, function (sprite, location) {
+    bubble.stick_to_wall(sprite, location)
+})
+controller.left.onEvent(ControllerButtonEvent.Repeated, function () {
+    bubble.tilt_angle(bubble.Choice.Left)
+})
+bubble.createBoard()
+bubble.load_bubble()
+game.setGameOverScoringType(game.ScoringType.HighScore)
